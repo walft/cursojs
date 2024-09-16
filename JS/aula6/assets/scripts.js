0bc870cb1ae47f2d294b72d62c51edcbba817631
walft <luishenriqueg@gmail.com>
2024-09-16 20:05:02 -0300

    up

---
+++ JS/aula6/assets/scripts.js
@@ -0,0 +1,18 @@
+let nomeCompleto = 'Luis';
+let Nomecompleto = 'Henrique';
+
+/*
+let nome
+nome = 'Luis'
+console.log(nome)
+nome = 'Henrique'
+console.log(nome)
+let nome = 'Pedro'
+
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+*/
