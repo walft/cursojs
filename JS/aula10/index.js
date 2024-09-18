ed64f3ed3bb1b8c925805c83c716d01e9dac72e1
walft <luishenriqueg@gmail.com>
2024-09-17 21:35:24 -0300

    up

---
+++ JS/aula10/index.js
@@ -0,0 +1,28 @@
+//STRINGS
+const nome = 'Luis';
+const nome1 = "Luis";
+const nome2 = `Luis`; // pode utilzar tamplates strings
+
+//NUMBERS
+const num1 = 10; // Number inteiro
+const num2 = 10.52; // Number real / flutuante
+
+//UNDEFINED
+let nomeAluno;
+
+//NULL
+let sobrenomeAluno = null
+
+//BOOLEAN
+const booleanF = false
+const booleanT = true
+
+console.log(typeof nome);
+console.log(typeof nome1);
+console.log(typeof nome2);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof nomeAluno);
+console.log(typeof sobrenomeAluno);
+console.log(typeof booleanF);
+console.log(typeof booleanT);
---
+++ JS/aula9/index.js
@@ -0,0 +1,12 @@
+// ECMAScript
+var nome = 'Luis';
+var nome = 'Henrique'
+console.log(nome);
+// É possível criar uma variável sem determinar se é var / let ou const mas não é para ser utilizada:
+sobreNome = 'Gonçalves';
+console.log(sobreNome);
+// LET não pode ser redeclado
+let idade = 34;
+console.log(idade);
+
+
