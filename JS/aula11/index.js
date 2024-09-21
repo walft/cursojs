70857addf3a1cca0407e1b85f0318367d7f008c8
walft <luishenriqueg@gmail.com>
2024-09-21 13:54:31 -0300

    uo

---
+++ JS/aula11/index.js
@@ -0,0 +1,37 @@
+/* Aritméticos
++ Adição e concatenação
+- Subtração
+/ Divisão
+* Multiplicação
+** Potenciação
+% Resto da divisão
+
+const num1 = 5;
+const num2 = 10;
+const soma = num1 * num2;
+console.log(num1 * num2);
+console.log(soma);
+
+
+let num1 = 10;
+let num2 = 30;
+let num3 = 100;
+
+console.log(num1 + num2 * num3);
+console.log((num1 + num2) * num3);
+
+
+
+let contador = 1;
+++ contador;
+console.log(contador);
+
+let descremento = 10
+-- descremento;
+console.log(descremento)
+
+*/
+
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
