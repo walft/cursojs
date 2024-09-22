b53571400eea579b86a0a390d9c71d06ae1aaa2a
walft <luishenriqueg@gmail.com>
2024-09-22 00:37:37 -0300

    up

--- JS/aula12/assets/scripts.js
+++ JS/aula12/assets/scripts.js
@@ -1,1 +1,8 @@
-alert('Com a nossa mensagem');
+alert ('Bem vindo ao site');
+let confirmacao = confirm('Você é maior de idade?');
+alert (`A sua afirmação foi ${confirmacao}! mas será mesmo?`);
+let anoNascimento = Number(prompt('Para ter certeza que você é maior de idade, me informe o ano do seu nascimento:'));
+alert (`Se estamos em 2024 - ${anoNascimento} acredito que...`);
+let idade = 2024 - anoNascimento;
+let maior = idade >= 18
+alert (`Você tem ${idade} anos de idade, sendo assim a afirmação que você é maior de idade é ${maior}`);
