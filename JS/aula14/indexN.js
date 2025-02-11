33614825ae4143739136b4f2bf8370afff932875
walft <luishenriqueg@gmail.com>
2025-02-10 21:30:44 -0300

    up

--- JS/aula14/indexN.js
+++ JS/aula14/indexN.js
@@ -1,7 +1,12 @@
 let num1 = 1500;
 let num2 = 2.5;
+let num3 = 10.69845720375454234;
+let num4 = 10;
 
 //console.log(num1.toString() + num2);
 //num1 = num1.toString();
-
-console.log(num1.toString(2));
+//console.log(num1.toString(2));
+//console.log(num3.toFixed(2));
+//console.log(Number.isInteger(num4));
+let temp = num2 * 'Olá';
+console.log (Number.isNaN(temp));
