738da5d7bfdbd2a1c7c658045f15250370e7d09e
walft <luishenriqueg@gmail.com>
2025-03-08 23:25:02 -0300

    up

--- JS/aula14/indexN.js
+++ JS/aula14/indexN.js
@@ -1,12 +1,11 @@
-let num1 = 1500;
-let num2 = 2.5;
-let num3 = 10.69845720375454234;
-let num4 = 10;
+let num1 = 0.7;
+let num2 = 0.1;
 
-//console.log(num1.toString() + num2);
-//num1 = num1.toString();
-//console.log(num1.toString(2));
-//console.log(num3.toFixed(2));
-//console.log(Number.isInteger(num4));
-let temp = num2 * 'Olá';
-console.log (Number.isNaN(temp));
+num1 += num2
+num1 += num2 
+num1 += num2 
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
---
+++ JS/aula15/script.js
@@ -0,0 +1,5 @@
+let num1 = 9.54789;
+let num2 = Math.round(num1);
+
+console.log(Math.max(1, 2, 3, 10, 1550, -10))
+console.log(Math.min(1, 2, 3, 10, 1550, -10))
