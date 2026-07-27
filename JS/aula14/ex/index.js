d2b2d8c1a395d25957d70cfb8838a26c202b7a7e
walft <luishenriqueg@gmail.com>
2026-07-26 22:41:50 -0300

    up

--- JS/aula14/ex/index.js
+++ JS/aula14/ex/index.js
@@ -1,2 +1,12 @@
 const nome = prompt('Digite seu nome completo: ');
-document.body.innerHTML += `Bem vindo ${nome}. <br />`;
+let tamanhoNome = length(nome);
+document.body.innerHTML += `Bem vindo ${nome}. <br />`; // inserir informações diretamente no Body, usar o += (recebe) para que ele não apague o que já tem no body.
+document.body.innerHTML += `Seu nome é: ${nome}<br />`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra LETRA no seu nome? ${nome.indexOf('a')}<br />`;
+document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome? ${nome.lastIndexOf('a')}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3, nome.length)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase}<br />`;
--- JS/aula14/indexN.js
+++ JS/aula14/indexN.js
@@ -1,11 +1,16 @@
-let num1 = 0.7;
-let num2 = 0.1;
+let num1 = 15.501239123;
+let num2 = 2.5;
 
-num1 += num2
-num1 += num2 
-num1 += num2 
+console.log(num1.toString() + num2); // O toString converte para string
+console.log(num1.toFixed(2)); //toFixed faz o arredondamento do número, inserir a quantidade de casas decimais
+console.log(Number.isInteger(num1)); // verifica se o numero é inteiro
+let temp = num1 * 'Olá';
+console.log(Number.isNaN(temp)); // verifica se o resultado for NaN
 
-num1 = parseFloat(num1.toFixed(2));
+let num3 = 0.7;
+let num4 = 0.1;
 
-console.log(num1);
-console.log(Number.isInteger(num1));
+console.log(num3 + num4); //O resultado é estranho, ele retorna 0.7999999999999999 ao invés de retornar 0.8
+num3 += num4;
+num3 = parseFloat(num3.toFixed(1)); // é necessário colocar o parce ou numberporque se não ele não é um numero de verdade
+console.log(num3, Number.isInteger(num3));
