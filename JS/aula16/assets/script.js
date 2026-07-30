c94796d5f5b0b9356bc0f455c0fbfd8f243d6c10
walft <luishenriqueg@gmail.com>
2026-07-29 23:22:42 -0300

    up

--- JS/aula16/assets/script.js
+++ JS/aula16/assets/script.js
@@ -1,2 +1,7 @@
+/* Recebe a entrada do usuário, seleciona os elementos do DOM e injeta o número digitado direto na página selecionando por ID, a localização fica salva em uma variável */
+
 const numero = prompt('Digite um número: ');
 const numeroBase = document.getElementById('numeroEscolhido');
+const texto = document.getElementById('texto');
+
+numeroBase.innerHTML = numero;
