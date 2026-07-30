54560aa122a2cd25c7328070c287bf177ad71285
walft <luishenriqueg@gmail.com>
2026-07-30 17:59:37 -0300

    up

--- JS/aula16/assets/script.js
+++ JS/aula16/assets/script.js
@@ -2,6 +2,19 @@
 
 const numero = prompt('Digite um número: ');
 const numeroBase = document.getElementById('numeroEscolhido');
-const texto = document.getElementById('texto');
+const raiz = document.getElementById('raiz');
+const inteiro = document.getElementById('inteiro');
+const Notnumber = document.getElementById('Notanumber');
+const baixo = document.getElementById('baixo');
+const cima = document.getElementById('cima');
+const duas = document.getElementById('duas')
 
 numeroBase.innerHTML = numero;
+raiz.innerHTML = numero ** (1/2);
+inteiro.innerHTML = Number.isInteger(Number(numero));
+Notnumber.innerHTML = Number.isNaN(Number(numero));
+baixo.innerHTML = Math.floor(Number(numero));
+cima.innerHTML = Math.ceil(Number(numero));
+duas.innerHTML = Number(numero).toFixed(2);
+
+
